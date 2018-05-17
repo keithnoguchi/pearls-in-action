86087a0d082ff6074807c408dfbaf024ed95b6a0
Kei Nohguchi <kei@nohguchi.com>
2018-05-16 17:39:13 -0700

    0x2: squash command for anagram problem

---
+++ 0x2/squash_main.c
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+#define WORDMAX	100
+	char word[WORDMAX], sig[WORDMAX], oldsig[WORDMAX];
+	int linenum = 0;
+
+	strcpy(oldsig, "");
+	while (scanf("%s %s", sig, word) != EOF) {
+		if (strcmp(oldsig, sig) != 0 && linenum > 0)
+			puts("");
+		strcpy(oldsig, sig);
+		linenum++;
+		printf("%s ", word);
+	}
+	puts("");
+	return 0;
+}
