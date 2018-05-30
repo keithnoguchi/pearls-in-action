a64f9232e4c0fdc9683c2531812e35eca4912ed0
Kei Nohguchi <kei@nohguchi.com>
2018-05-30 00:32:48 -0700

    0xb/qsort3: Added randomized version of the simple qsort
    It's fast for the sorted array, but doesn't help in case of
    the identical array.  This shows qsort6, a bit complicated
    version of the quick sort with random pivot pick gives us
    a better overall, random, identical, and sorted array, run
    time.

--- 0xb/main_test.c
+++ 0xb/main_test.c
@@ -7,6 +7,7 @@ extern int test_isort2_random(int *i);
 extern int test_isort3_random(int *i);
 extern int test_qsort1_random(int *i);
 extern int test_qsort2_random(int *i);
+extern int test_qsort3_random(int *i);
 extern int test_qsort4_random(int *i);
 extern int test_qsort5_random(int *i);
 extern int test_qsort6_random(int *i);
@@ -15,6 +16,7 @@ extern int test_isort2_identical(int *i);
 extern int test_isort3_identical(int *i);
 extern int test_qsort1_identical(int *i);
 extern int test_qsort2_identical(int *i);
+extern int test_qsort3_identical(int *i);
 extern int test_qsort4_identical(int *i);
 extern int test_qsort5_identical(int *i);
 extern int test_qsort6_identical(int *i);
@@ -23,6 +25,7 @@ extern int test_isort2_sorted(int *i);
 extern int test_isort3_sorted(int *i);
 extern int test_qsort1_sorted(int *i);
 extern int test_qsort2_sorted(int *i);
+extern int test_qsort3_sorted(int *i);
 extern int test_qsort4_sorted(int *i);
 extern int test_qsort5_sorted(int *i);
 extern int test_qsort6_sorted(int *i);
@@ -46,6 +49,8 @@ int main(void)
 		fail++;
 	if (test_qsort2_random(&i))
 		fail++;
+	if (test_qsort3_random(&i))
+		fail++;
 	if (test_qsort4_random(&i))
 		fail++;
 	if (test_qsort5_random(&i))
@@ -62,6 +67,8 @@ int main(void)
 		fail++;
 	if (test_qsort2_identical(&i))
 		fail++;
+	if (test_qsort3_identical(&i))
+		fail++;
 	if (test_qsort4_identical(&i))
 		fail++;
 	if (test_qsort5_identical(&i))
@@ -77,6 +84,8 @@ int main(void)
 	if (test_qsort1_sorted(&i))
 		fail++;
 	if (test_qsort2_sorted(&i))
+		fail++;
+	if (test_qsort3_sorted(&i))
 		fail++;
 	if (test_qsort4_sorted(&i))
 		fail++;
---
+++ 0xb/qsort3.c
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <time.h>
+#include <stdlib.h>
+
+static void swap(int x[], unsigned int a, unsigned int b)
+{
+	int tmp = x[a];
+	x[a] = x[b];
+	x[b] = tmp;
+}
+
+static void __qsort(int x[], unsigned int l, unsigned int u)
+{
+	int i, m;
+
+	if (u-l <= 1)
+		return;
+
+	/* pick the random pivot */
+	swap(x, l, l+rand()%(u-l));
+	m = l;
+	for (i = l+1; i < u; i++)
+		if (x[i] < x[l]) {
+			int tmp = x[++m];
+			x[m] = x[i];
+			x[i] = tmp;
+		}
+	swap(x, l, m);
+	__qsort(x, l, m);
+	__qsort(x, m+1, u);
+}
+
+void qsort3(int x[], unsigned int nr)
+{
+	__qsort(x, 0, nr);
+}
