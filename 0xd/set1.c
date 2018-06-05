5e44323c009197add88513613de635b52ce7d69c
Kei Nohguchi <kei@nohguchi.com>
2018-06-04 17:47:32 -0700

    0xd/set1: Put the sentinel in the front

--- 0xd/set1.c
+++ 0xd/set1.c
@@ -1,13 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "set.h"
 
 struct set1 {
-	struct set	base;
 	int		current;
+	struct set	base;
 	int		x[];
 };
 
@@ -32,9 +31,9 @@ struct set *alloc_set1(int nr, int max)
 		return NULL;
 
 	s->current = 0;
-	for (i = 0; i < nr+1; i++)
-		s->x[i] = 0;
-	s->x[nr] = max; /* sentinel */
 	s->base.ops = &ops;
+	for (i = 0; i < nr; i++)
+		s->x[i] = 0;
+	s->x[0] = max; /* sentinel */
 	return &s->base;
 }
